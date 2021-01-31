21a4aab4f5225aaeeec687947038ed05610a9e39
Abhishec177 <abhishek122k@gmail.com>
2021-01-31 13:39:17 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Layout from '../components/Layout';
+
+class Index extends Component {
+  render() {
+    return(
+      <Layout>
+        Hello
+      </Layout>
+    );
+  }
+}
+
+export default Index;
